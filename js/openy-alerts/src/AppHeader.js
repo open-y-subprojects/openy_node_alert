f77062415fd198cdbce6f90ddd87b7593d5d49e2
shuklina <lera.shuklina@jet.dev>
2022-02-01 11:01:44 +0200

    Fix arrow in alert

--- js/openy-alerts/src/AppHeader.js
+++ js/openy-alerts/src/AppHeader.js
@@ -129,7 +129,7 @@ class App extends Component {
                   data-role="none"
                   className={nextArrowClasses}
                   role="button"
-                  aria-disabled="false"
+                  aria-disabled="true"
                   aria-label="Next alert message"
                   onClick={this.next}
                 >
