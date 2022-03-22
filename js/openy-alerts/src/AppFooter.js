0cbdb21049bcf3d4144ac36b68fe53c0ec963096
Vasyl Manachynskyi <vasyl.manachynskyi@fivejars.com>
2022-03-22 16:42:30 +0200

    [YP4-18] Move to previous slide on close.

--- js/openy-alerts/src/AppFooter.js
+++ js/openy-alerts/src/AppFooter.js
@@ -70,6 +70,7 @@ class App extends Component {
                   bgColor={a.bgColor}
                   focus={this.goto}
                   index={idx++}
+                  slider={this.slider}
                 />
               );
             } else {
